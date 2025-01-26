556e2dd5a38c9f476355f27454017f18d90ef043
BMedvedev <bgdnmdvdv@gmail.com>
2025-01-26 13:43:16 +0300

    add API

--- core/core.py
+++ core/core.py
@@ -1,7 +1,17 @@
-from fastapi import HTTPException
-from databases_queries.declarative import DeclarativeSQLQuery
+import logging
+from fastapi import HTTPException, status
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload, joinedload, load_only
+
+from databases_queries import depends_session
 from fastapi import APIRouter
 
+from models.declarative_models import WorkersOrm, ResumesOrm, VacanciesOrm
+from models.schemas import WorkersAddDTO, M2MResumesVacanciesDTO, WorkersRelDTO, ResumesDTO, WorkersDTO
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
 
 core_router = APIRouter()
 
@@ -11,8 +21,19 @@ core_router = APIRouter()
     tags=['Работники'],
     summary='Получение списка работников'
 )
-async def get_workers():
-    return await DeclarativeSQLQuery.convert_workers_to_dto()
+async def get_workers(session: depends_session):
+    query = (
+        select(WorkersOrm)
+        .options(
+            load_only(WorkersOrm.id, WorkersOrm.username),
+            selectinload(WorkersOrm.resumes)
+        )
+        .limit(3)
+    )
+    res = await session.execute(query)
+    result_orm = res.scalars().all()
+    response = [WorkersRelDTO.model_validate(row, from_attributes=True) for row in result_orm]
+    return response
 
 
 @core_router.get(
@@ -20,32 +41,43 @@ async def get_workers():
     tags=['Работники'],
     summary='Получение списка резюме'
 )
-async def get_workers():
-    return await DeclarativeSQLQuery.select_resumes_with_all_relationships()
+async def get_resumes(session: depends_session):
+    query = (
+        select(ResumesOrm)
+        .options(joinedload(ResumesOrm.worker))
+        .options(selectinload(ResumesOrm.vacancies_replied).load_only(VacanciesOrm.title))
+    )
+    res = await session.execute(query)
+    result_orm = res.unique().scalars().all()
+    result_dto = [M2MResumesVacanciesDTO.model_validate(row, from_attributes=True) for row in result_orm]
+    return result_dto
 
 
-# @app.post(
-#     path='/books',
-#     tags=['Книги'],
-#     summary='Создание книги'
-# )
-# def create_book(new_book: NewBook):
-#     books.append(
-#         {
-#             'id': len(books) + 1,
-#             'title': new_book.title,
-#             'author': new_book.author,
-#         }
-#     )
-#     return new_book
-#
-# @app.get(
-#     path='/books/{book_id}',
-#     tags=['Книги'],
-#     summary='Получение книги по идентификатору'
-# )
-# def get_book(book_id: int):
-#     for book in books:
-#         if book['id'] == book_id:
-#             return book
-#     raise HTTPException(404, 'нет')
+@core_router.post(
+    path='/workers',
+    tags=['Работники'],
+    summary='Добавление работника',
+    status_code=status.HTTP_201_CREATED
+)
+async def create_worker(data: WorkersAddDTO, session: depends_session):
+    worker = WorkersOrm(
+        **data.model_dump()
+    )
+    session.add(worker)
+    await session.flush()
+    logger.debug(f'Добавлен работник с ID: {worker.id}')
+    await session.commit()
+    return 'OK'
+
+
+@core_router.get(
+    path='/resumes/{resume_id}',
+    tags=['Работники'],
+    summary='Получение резюме по идентификатору'
+)
+async def get_resume(resume_id: int, session: depends_session):
+    result = await session.get(entity=ResumesOrm, ident=resume_id)
+    if not result:
+        raise HTTPException(status_code=404, detail=f"Резюме с идентификатором {resume_id} не найдено")
+    response = ResumesDTO.model_validate(result, from_attributes=True)
+    return response
--- databases_queries/__init__.py
+++ databases_queries/__init__.py
@@ -1,3 +1,6 @@
+from typing import Annotated
+
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from config import database_settings
 
@@ -6,3 +9,13 @@ engine = create_async_engine(
     url=database_settings.database_url,
     echo=True
 )
+
+session_factory = async_sessionmaker(engine, expire_on_commit=True)
+
+async def get_session():
+    """Сессия для взаимодействия с БД"""
+    async with session_factory() as session:
+        yield session
+
+
+depends_session = Annotated[AsyncSession, Depends(get_session)]
